ecc38ba78d883e4dd0e93c3f670100a749866045
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2024-01-23 21:11:59 -0300

    mvp testado

---
+++ index.py
@@ -0,0 +1,6 @@
+resposta = input("O que é, o que é: quanto mais seca, mais molhado fica?")
+
+if resposta == "toalha":
+  print("Resposta correta, você ganhou!")
+else:
+  print("Que pena, a resposta correta era 'toalha' ")
