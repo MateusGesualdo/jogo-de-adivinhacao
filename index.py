56d1514ccb81a95af42e96c2dc8b0b84ecbff6e4
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2024-01-23 21:18:02 -0300

    variável criada para a pergunta

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-resposta = input("O que é, o que é: quanto mais seca, mais molhado fica?")
+pergunta = "O que é, o que é: quanto mais seca, mais molhado fica?"
+
+resposta = input(pergunta)
 
 if resposta == "toalha":
   print("Resposta correta, você ganhou!")
 else:
   print("Que pena, a resposta correta era 'toalha' ")
+
